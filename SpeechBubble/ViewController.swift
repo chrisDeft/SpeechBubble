2b4d843af879a685f0413e0f21ee8f89630af362
chrisDeft <chris@deftapplications.com>
2017-10-05 16:25:16 +0100

    Initial Commit

---
+++ SpeechBubble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechBubble
+//
+//  Created by Chris Adamson on 05/10/2017.
+//  Copyright © 2017 Chris Adamson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
